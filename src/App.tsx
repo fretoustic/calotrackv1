501e3eba1ca241c3f26ec9384d8e11290cf90bc1
Harsh Preet Singh <harshpreetsingh03@hotmail.com>
2025-01-22 14:29:43 +0530

    took care of styling and added global css to get rid of margins all around

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SignUp from './SignUp'
-import SignUp2 from './SignUp2'
-import SignUp3 from './SignUp3'
-import HomePage from './homePage'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import SignUp2 from "./SignUp2";
+import SignUp3 from "./SignUp3";
+import HomePage from "./homePage";
+import "./global.css";
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/signup2" element={<SignUp2 />} />
-          <Route path="/signup3" element={<SignUp3 />} />
-          <Route path="/homepage" element={<HomePage/>} />
-        </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signup2" element={<SignUp2 />} />
+        <Route path="/signup3" element={<SignUp3 />} />
+        <Route path="/homepage" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

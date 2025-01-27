161525f79155b09045bb550cf031b9ceac03012b
Harsh Preet Singh <harshpreetsingh03@hotmail.com>
2025-01-27 10:16:39 +0530

    removed unused imports

--- src/components/charts/Chart.tsx
+++ src/components/charts/Chart.tsx
@@ -1,14 +1,7 @@
 import { Pie } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  ArcElement,
-  Tooltip,
-  Legend,
-  Colors,
-} from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import "../homepage/homePage.css";
 import { useThemeStore } from "../../store";
-import { blue } from "@mui/material/colors";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 interface Props {
